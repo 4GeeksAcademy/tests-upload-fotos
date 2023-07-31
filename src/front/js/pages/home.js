223368c8ad546963d554e5e3e1f038709720f390
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-07-31 18:27:40 +0000

    first commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,39 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import React, { useContext, useState } from "react";
+import Axios from 'axios';
+import {Image} from 'cloudinary-react'
+import {Cloudinary} from "@cloudinary/url-gen";
+
 import "../../styles/home.css";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
+export const UploadImg = () => {
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
-};
+	const cld = new Cloudinary({cloud: {cloudName: 'dbxeaqsv4'}});
+
+    const [imageSelected, setImageSelected] = useState ("")
+
+    const uploadImage = (files) =>{
+        console.log(files[0])
+        const formData = new FormData();
+        formData.append("file", imageSelected);
+        formData.append("uploader", "dbxeaqsv4");
+
+        cld.v2.upload (
+            "https://api.cloudinary.com/v1_1/dbxeaqsv4/image/upload", formData) 
+            .then((response) => {
+            console.log(response);
+            });
+    };
+
+    return ( 
+        <div>
+            <input 
+            type="file" 
+            onChange={(e) =>{setImageSelected(e.target.files[0]);
+            }} />
+
+            <button onClick={uploadImage}>upload image</button>
+
+            <Image cloudName="dbxeaqsv4" publicId=""/>
+        </div>
+    )
+}
